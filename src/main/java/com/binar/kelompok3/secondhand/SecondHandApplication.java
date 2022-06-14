dbc0cbfc2f64ac4204cae91f40f129bb8b324c1f
lonelykatana <erickgultom325@gmail.com>
2022-06-15 00:51:59 +0700

    First commit 15/06. Setup spring

---
+++ src/main/java/com/binar/kelompok3/secondhand/SecondHandApplication.java
@@ -0,0 +1,14 @@
+package com.binar.kelompok3.secondhand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondHandApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecondHandApplication.class, args);
+        System.out.println("Hello World!");
+    }
+
+}
