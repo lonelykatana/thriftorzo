eb12c9a243ac79d4518a46a7ba9cd2f81d54ae50
lonelykatana <erickgultom325@gmail.com>
2022-06-15 18:26:18 +0700

    15/06 | Selesai membuat model dan repo users, products, dan category

---
+++ src/main/java/com/binar/kelompok3/secondhand/model/Products.java
@@ -0,0 +1,44 @@
+package com.binar.kelompok3.secondhand.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "products", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "id")
+})
+public class Products implements Serializable {
+
+    private static final long serialVersionUID = 1865643894L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "price")
+    private Double price;
+
+    @Column(name = "status")
+    private Integer status;
+
+    @Column(name = "description", length = 500)
+    private String description;
+
+    //Entity Category belum dibuat
+//    @ManyToOne(fetch = FetchType.EAGER)
+//    @Column(name = "category_id")
+//    private Category categoryId;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    private Users userID;
+}
