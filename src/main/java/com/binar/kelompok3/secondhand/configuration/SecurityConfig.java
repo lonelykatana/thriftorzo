48827a0a8e0547f353b010324737342cdfc52cb2
Blaskoasky <iriantomauduta@gmail.com>
2022-06-28 22:13:03 +0700

    28/06 | fix auth

--- src/main/java/com/binar/kelompok3/secondhand/configuration/SecurityConfig.java
+++ src/main/java/com/binar/kelompok3/secondhand/configuration/SecurityConfig.java
@@ -60,14 +60,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeRequests()
                 // .antMatchers("/**").permitAll()
-                .antMatchers("/swagger-ui/index.html", "/Secondhand-docs.html").permitAll()
+                .antMatchers("/swagger-ui/index.html").permitAll()
+                .antMatchers("/Secondhand-docs.html").permitAll()
                 .antMatchers("/auth/**").permitAll()
-                .antMatchers("/users/**").hasAnyAuthority(buyer, seller)
+                .antMatchers("/user/**").hasAnyAuthority(buyer, seller)
                 .antMatchers("/image").hasAnyAuthority(buyer, seller)
+                .antMatchers("/product").hasAnyAuthority(buyer, seller) // Masalah get all product untuk homepage
                 .antMatchers("/wishlist").hasAnyAuthority(buyer, seller);
-
-        //.anyRequest().authenticated();
-
         http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 
