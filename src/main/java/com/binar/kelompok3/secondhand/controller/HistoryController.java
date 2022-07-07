bcbf9c1991f835248763264405218ab347441aef
Blaskoasky <iriantomauduta@gmail.com>
2022-07-07 22:49:41 +0700

    07/07 | new TransactionResponse merged to one buyer/seller

--- src/main/java/com/binar/kelompok3/secondhand/controller/HistoryController.java
+++ src/main/java/com/binar/kelompok3/secondhand/controller/HistoryController.java
@@ -1,8 +1,7 @@
 package com.binar.kelompok3.secondhand.controller;
 
 import com.binar.kelompok3.secondhand.model.entity.Offers;
-import com.binar.kelompok3.secondhand.model.response.offers.OfferResponseBuyer;
-import com.binar.kelompok3.secondhand.model.response.offers.OfferResponseSeller;
+import com.binar.kelompok3.secondhand.model.response.offers.TransactionResponse;
 import com.binar.kelompok3.secondhand.service.offers.IOffersService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -21,20 +20,20 @@ public class HistoryController {
     IOffersService iOffersService;
 
     @GetMapping("/buyer-history/{userId}")
-    public ResponseEntity<List<OfferResponseBuyer>> getBuyerHistory(@PathVariable Integer userId) {
+    public ResponseEntity<List<TransactionResponse>> getBuyerHistory(@PathVariable Integer userId) {
         List<Offers> offers = iOffersService.getAllByUserId(userId);
-        List<OfferResponseBuyer> offerResponses = offers.stream()
-                .map(OfferResponseBuyer::new)
+        List<TransactionResponse> offerResponses = offers.stream()
+                .map(TransactionResponse::new)
                 .collect(Collectors.toList());
 
         return new ResponseEntity<>(offerResponses, HttpStatus.OK);
     }
 
     @GetMapping("/seller-history/{userId}")
-    public ResponseEntity<List<OfferResponseSeller>> getHistorySeller(@PathVariable Integer userId) {
+    public ResponseEntity<List<TransactionResponse>> getHistorySeller(@PathVariable Integer userId) {
         List<Offers> offers = iOffersService.getHistorySeller(userId);
-        List<OfferResponseSeller> offerResponses = offers.stream()
-                .map(OfferResponseSeller::new)
+        List<TransactionResponse> offerResponses = offers.stream()
+                .map(TransactionResponse::new)
                 .collect(Collectors.toList());
 
         return new ResponseEntity<>(offerResponses, HttpStatus.OK);
