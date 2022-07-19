59239c29f64cac0b3f8d1033628ee2890a71569e
Blaskoasky <iriantomauduta@gmail.com>
2022-07-19 22:02:38 +0700

    19/07 | merge

--- src/main/java/com/binar/kelompok3/secondhand/configuration/SwaggerConfig.java
+++ src/main/java/com/binar/kelompok3/secondhand/configuration/SwaggerConfig.java
@@ -17,7 +17,8 @@ public class SwaggerConfig {
 
     @Bean
     public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo())
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.binar.kelompok3.secondhand.controller"))
                 .build();
