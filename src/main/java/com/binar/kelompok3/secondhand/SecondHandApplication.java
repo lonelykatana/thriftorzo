77d3930b46abb5ba5b75c7f99a294e93503c48db
lonelykatana <erickgultom325@gmail.com>
2022-07-25 00:02:22 +0700

    24/07 Yang udah bagus

--- src/main/java/com/binar/kelompok3/secondhand/SecondHandApplication.java
+++ src/main/java/com/binar/kelompok3/secondhand/SecondHandApplication.java
@@ -4,11 +4,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 
 @SpringBootApplication
-//@EnableJpaAuditing
 public class SecondHandApplication {
 
     private static final Logger logger = LoggerFactory.getLogger(SecondHandApplication.class);
--- src/test/java/com/binar/kelompok3/secondhand/service/products/ProductsServiceImplTest.java
+++ src/test/java/com/binar/kelompok3/secondhand/service/products/ProductsServiceImplTest.java
@@ -19,7 +19,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-class IProductsServiceTest {
+class ProductsServiceImplTest {
 
     @Mock
     private ProductsRepository repository;
